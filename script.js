f3b8bb06de02e96b88e56e12ba8141ac3a3f32b7
Andrew <apd05@uw.edu>
2018-07-30 19:19:58 -0700

    created two charts on one page,

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ console.log("let's build some charts!")
       // Set a callback to run when the Google Visualization API is loaded.
       
       google.charts.setOnLoadCallback(drawChart); 
+      google.charts.setOnLoadCallback(drawAllocationChart);
+
 	   // this draws chart on page load
+
 
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
@@ -34,6 +37,32 @@ console.log("let's build some charts!")
 
         // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+
+
+      
+      function drawAllocationChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Score Range'); 
+        // data type and what it's calling
+
+        data.addColumn('number', 'Games');
+        data.addRows([
+          ['0-100', 2],
+          ['100-200', 5],
+          ['200-300', 3]
+        ]);
+
+        // Set chart options
+        var options = {'title':'Bowling Scores',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('allocations'));
         chart.draw(data, options);
       }
 
